9c7c47f4d0c5b059b27c111bba29f89b4a8e277f
tauri_s <tauristclaire@gmail.com>
2022-08-23 18:59:38 -0700

    Fetched API for random users

--- js/script.js
+++ js/script.js
@@ -1,1 +1,9 @@
 const randomFolks = document.querySelector(".random-peeps");
+
+const getData = async function () {
+    const usersRequest = await fetch("https://randomuser.me/api?results=5");
+    const data = await usersRequest.json();
+    console.log(data);
+};
+
+getData();
