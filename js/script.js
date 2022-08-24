032ad75232a04d5c9e1281f96ede69cdb553f619
tauri_s <tauristclaire@gmail.com>
2022-08-24 16:52:24 -0700

    Display 5 users

--- js/script.js
+++ js/script.js
@@ -3,7 +3,28 @@ const randomFolks = document.querySelector(".random-peeps");
 const getData = async function () {
     const usersRequest = await fetch("https://randomuser.me/api?results=5");
     const data = await usersRequest.json();
-    console.log(data);
+    //console.log(data);
+    //Drill down to array of user objects and map it to results
+    const userResults = data.results;
+    displayUsers(userResults);
 };
 
 getData();
+
+const displayUsers = function (userResults) {
+    //Make sure you don't duplicate any dom elements
+    randomFolks.innerHTML = "";
+
+    for (const user of userResults) {
+        const country = user.location.country;
+        const name = user.name.first;
+        const imageUrl = user.picture.medium;
+        const userDiv = document.createElement("div");
+        userDiv.innerHTML = `
+        <h3>${name}</h3>
+        <p>${country}</p>
+        <img src=${imageUrl} alt="User avatar" />
+    `;
+randomFolks.append(userDiv);
+    };
+};
